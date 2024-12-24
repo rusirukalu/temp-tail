631d0489710da0e003337e9bfe00b223bb239cd8
Rusiru Salwathura <rusirusalwathura@icloud.com>
2024-12-25 03:05:45 +0530

    navbar to login link updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -189,7 +189,7 @@ const Navbar = () => {
               </div>
               <div className="py-6">
                 <a
-                  href="#"
+                  href="/login"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold text-gray-900 hover:bg-gray-50"
                 >
                   Log in
